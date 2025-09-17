0fd41526388cbe21c16d339be8c3a0b641a57900
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-09-17 12:34:04 +0530

    Occusearch API added in page

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ const mjData = require('./MJ/API.json');
 const resolutionProData = require('./Resolution-Pro/API.json');
 const resolutionProPerformanceTestData = require('./Resolution-Pro/performace.json');
 
+const occuSearchAPITestData = require('./Occusearch/api.data.json');
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// ✅ Allow CORS for everyone
 app.use(cors({
   origin: '*',  // allow all domains
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: false
 }));
 
-// ✅ API route
 app.get('/api/asme', (req, res) => {
   res.json(asmeData);
 });
@@ -38,11 +38,14 @@ app.get('/api/resolution-pro', (req, res) => {
 });
 
 app.get('/api/resolution-pro-performance', (req, res) => {
-  res.json(resolutionProPerformanceTestData); 
+  res.json(resolutionProPerformanceTestData);
 });
 
-// ✅ Start server
+app.get('/api/occu-search/api/test/data', (req, res) => {
+  req.json(occuSearchAPITestData);
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
